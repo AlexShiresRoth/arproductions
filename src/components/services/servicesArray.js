966f5def7ee787f19b596371bdc6a98065e35dc0
Alex Roth <alexroth96@gmail.com>
2020-02-27 14:59:04 -0500

    add service img slider

--- src/components/services/servicesArray.js
+++ src/components/services/servicesArray.js
@@ -1,58 +1,36 @@
-import React from 'react';
+import React from "react";
 export const servicesArray = [
-	{
-		title: 'Responsively Designed Sites.',
-		text: `Each website is built to be responsive. Responsive design means that your site will look good in different devices: tablets, mobile devices and desktops. `,
-		img: (
-			<img
-				src="https://res.cloudinary.com/snackmanproductions/image/upload/v1582658614/business%20site/responsive-gif_kktmq9.gif"
-				alt="gif"
-			></img>
-		),
-		imgs: null,
-	},
-	{
-		title: 'Unique Layouts.',
-		text: `To make your business standout, you need a custom layout separate from all the cookie-cutter
+  {
+    title: "Responsively Designed Sites.",
+    text: `Each website is built to be responsive. Responsive design means that your site will look good in different devices: tablets, mobile devices and desktops. `,
+    img: (
+      <img
+        src="https://res.cloudinary.com/snackmanproductions/image/upload/v1582658614/business%20site/responsive-gif_kktmq9.gif"
+        alt="gif"
+      ></img>
+    ),
+    imgs: null
+  },
+  {
+    title: "Unique Layouts.",
+    text: `To make your business standout, you need a custom layout separate from all the cookie-cutter
 							templates by the likes of Wix, Squarespace, Godaddy, etc. I build unique sites from scratch,
                             tailored to your business' needs and interests.`,
-		img: (
-			<img
-				src="https://res.cloudinary.com/snackmanproductions/image/upload/c_scale,q_51,w_994/v1582755367/business%20site/2020-02-26_p0vg8y.png"
-				alt="pic"
-			></img>
-		),
-		imgs: [
-			{
-				img: (
-					<img
-						src="https://res.cloudinary.com/snackmanproductions/image/upload/v1581450719/portfolio/2020-02-11_ii3g55.png"
-						alt="pic"
-					></img>
-				),
-			},
-			{
-				img: (
-					<img
-						src="https://res.cloudinary.com/snackmanproductions/image/upload/v1580442667/portfolio/2019-10-29_kzfuqc.png"
-						alt="pic"
-					></img>
-				),
-			},
-			{
-				img: (
-					<img
-						src="https://res.cloudinary.com/snackmanproductions/image/upload/v1575660519/portfolio/2019-12-06_1_awxsjf.png"
-						alt="pic"
-					></img>
-				),
-			},
-		],
-	},
-	{
-		title: 'Interactive Experiences',
-		text: `Each site is developed to maintain user engagement, with simple and easy to use user interfaces. `,
-		img: null,
-		imgs: null,
-	},
+    img: null,
+    imgs: [
+      "https://res.cloudinary.com/snackmanproductions/image/upload/c_scale,q_51,w_994/v1582755367/business%20site/2020-02-26_p0vg8y.png",
+
+      "https://res.cloudinary.com/snackmanproductions/image/upload/v1581450719/portfolio/2020-02-11_ii3g55.png",
+
+      "https://res.cloudinary.com/snackmanproductions/image/upload/v1580442667/portfolio/2019-10-29_kzfuqc.png",
+
+      "https://res.cloudinary.com/snackmanproductions/image/upload/v1575660519/portfolio/2019-12-06_1_awxsjf.png"
+    ]
+  },
+  {
+    title: "Interactive Experiences",
+    text: `Each site is developed to maintain user engagement, with simple and easy to use user interfaces. `,
+    img: null,
+    imgs: null
+  }
 ];
