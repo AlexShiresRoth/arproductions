79de582834aeff3e54200654fb141fbe210475b0
Alex Rothenberg <alexroth96@gmail.com>
2020-02-26 20:17:28 -0500

    work on services, add btn animation.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,46 +1,45 @@
-import React from "react";
-import headerStyle from "./Header.module.scss";
-import { headerSvg } from "./headerSvg";
+import React from 'react';
+import headerStyle from './Header.module.scss';
+import { headerSvg } from './headerSvg';
 const Header = () => {
-  const borderSvg = (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-      <path
-        fill-opacity="1"
-        d="M0,128L80,112C160,96,320,64,480,69.3C640,75,800,117,960,122.7C1120,128,1280,96,1360,80L1440,64L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"
-      ></path>
-    </svg>
-  );
-  const borderBtm = (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-      <path
-        fill-opacity="1"
-        d="M0,160L80,186.7C160,213,320,267,480,266.7C640,267,800,213,960,165.3C1120,117,1280,75,1360,53.3L1440,32L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
-      ></path>
-    </svg>
-  );
-  return (
-    <header className={headerStyle.header}>
-      <div className={headerStyle.text__box}>
-        <div className={headerStyle.inner}>
-          <h1>
-            AlexRoth <br /> productions.
-          </h1>
-          <p>Professional Web Development.</p>
-          <div className={headerStyle.services}>
-            <p>Responsive Design.</p>
-            <p>Unique Layouts.</p>
-            <p>Interactive Experiences.</p>
-          </div>
-        </div>
-      </div>
+	const borderSvg = (
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+			<path
+				fill-opacity="1"
+				d="M0,128L80,112C160,96,320,64,480,69.3C640,75,800,117,960,122.7C1120,128,1280,96,1360,80L1440,64L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"
+			></path>
+		</svg>
+	);
+	const borderBtm = (
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+			<path
+				fill-opacity="1"
+				d="M0,160L80,186.7C160,213,320,267,480,266.7C640,267,800,213,960,165.3C1120,117,1280,75,1360,53.3L1440,32L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+			></path>
+		</svg>
+	);
+	return (
+		<header className={headerStyle.header}>
+			<div className={headerStyle.text__box}>
+				<div className={headerStyle.inner}>
+					<h1>
+						AlexRoth <br /> productions.
+					</h1>
+					<p>Professional Web Development.</p>
+					<div className={headerStyle.services}>
+						<button>Contact</button>
+						<button>See Work</button>
+					</div>
+				</div>
+			</div>
 
-      <div className={headerStyle.svg__container}>
-        {headerSvg}
-        <div className={headerStyle.border__top}>{borderSvg}</div>
-        <div className={headerStyle.border__btm}>{borderBtm}</div>
-      </div>
-    </header>
-  );
+			<div className={headerStyle.svg__container}>
+				{headerSvg}
+				<div className={headerStyle.border__top}>{borderSvg}</div>
+				<div className={headerStyle.border__btm}>{borderBtm}</div>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -1,45 +1,51 @@
-import React from "react";
-import PropTypes from "prop-types";
-import serviceStyle from "./Services.module.scss";
-import { servicesArray } from "./servicesArray";
+import React from 'react';
+import PropTypes from 'prop-types';
+import serviceStyle from './Services.module.scss';
+import { servicesArray } from './servicesArray';
 
 const Services = props => {
-  const serviceSvg = (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-      <path
-        fill="#400082"
-        fill-opacity="1"
-        d="M0,192L60,170.7C120,149,240,107,360,101.3C480,96,600,128,720,154.7C840,181,960,203,1080,186.7C1200,171,1320,117,1380,90.7L1440,64L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"
-      ></path>
-    </svg>
-  );
-  const services = servicesArray.map((service, i) => {
-    return (
-      <div className={serviceStyle.grid}>
-        <div className={serviceStyle.col}>
-          <div
-            className={serviceStyle.text__box}
-            style={i % 2 !== 0 ? { order: "1" } : { order: "0" }}
-          >
-            <div className={serviceStyle.inner}>
-              <h3>{service.title}</h3>
-              <p>{service.text}</p>
-            </div>
-          </div>
-          <div className={serviceStyle.img__container}>{service.img}</div>
-        </div>
-      </div>
-    );
-  });
-  return (
-    <section className={serviceStyle.section}>
-      {serviceSvg}
-      <div className={serviceStyle.heading}>
-        <h2>Need a more impressive web presence?</h2>
-      </div>
-      {services}
-    </section>
-  );
+	const serviceSvg = (
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+			<path
+				fill="#400082"
+				fill-opacity="1"
+				d="M0,192L60,170.7C120,149,240,107,360,101.3C480,96,600,128,720,154.7C840,181,960,203,1080,186.7C1200,171,1320,117,1380,90.7L1440,64L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"
+			></path>
+		</svg>
+	);
+	const services = servicesArray.map((service, i) => {
+		return (
+			<div className={serviceStyle.grid}>
+				<div className={serviceStyle.col}>
+					<div className={serviceStyle.col_grid} style={i % 2 !== 0 ? { background: 'transparent' } : null}>
+						<div className={serviceStyle.inner_left} style={i % 2 !== 0 ? { order: '1' } : { order: '0' }}>
+							<h3>{service.title}</h3>
+							<p>{service.text}</p>
+						</div>
+						<div
+							className={service.imgs !== null ? serviceStyle.inner__img__grid : serviceStyle.inner_right}
+						>
+							{service.img}
+							{service.imgs !== null ? (
+								<div class={serviceStyle.grid__inner}>
+									{service.imgs !== null ? service.imgs.map(val => val.img) : null}
+								</div>
+							) : null}
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	});
+	return (
+		<section className={serviceStyle.section}>
+			{serviceSvg}
+			<div className={serviceStyle.heading}>
+				<h2>Need a more impressive web presence?</h2>
+			</div>
+			{services}
+		</section>
+	);
 };
 
 Services.propTypes = {};
