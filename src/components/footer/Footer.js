f092ae6b8a4b24fd5dcaf72c20e615540643409c
Alex Roth <alexroth96@gmail.com>
2020-03-12 17:55:37 -0400

    add links to footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import footerStyle from './Footer.module.scss';
+import {
+  TiSocialInstagram,
+  TiSocialFacebookCircular,
+  TiSocialTwitterCircular
+} from 'react-icons/ti';
 const Footer = props => {
-  return <footer className={footerStyle.footer}></footer>;
+  return (
+    <footer className={footerStyle.footer}>
+      <div className={footerStyle.grid}>
+        <div className={footerStyle.col}>
+          <p>AlexRoth productions &copy; 2020</p>
+        </div>
+        <div className={footerStyle.col}>
+          <TiSocialInstagram />
+          <TiSocialFacebookCircular />
+          <TiSocialTwitterCircular />
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 Footer.propTypes = {};
