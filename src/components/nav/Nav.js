bd8a603705ee11d01ad72eac8b5cab7bd3b1e980
Alex Rothenberg <alexroth96@gmail.com>
2020-03-21 16:12:41 -0400

    add header style for mobile

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -3,81 +3,102 @@ import navStyles from './Nav.module.scss';
 import { connect } from 'react-redux';
 
 const Nav = ({ refs: { refs, active } }) => {
-  const [isMobile, setMobile] = useState(false);
+	const [isMobile, setMobile] = useState(false);
+	const [navState, toggleNav] = useState(false);
 
-  const setResize = e =>
-    window.addEventListener('resize', e => setMobile(window.innerWidth <= 700));
+	const setResize = e => window.addEventListener('resize', e => setMobile(window.innerWidth <= 700));
 
-  useEffect(() => {
-    setResize();
-  }, [setResize]);
+	useEffect(() => {
+		setResize();
+	}, []);
 
-  const scrollToSections = ref => {
-    window.scrollTo({
-      top: ref ? ref.current.offsetTop : 0,
-      left: 0,
-      behavior: 'smooth'
-    });
-  };
+	const scrollToSections = ref => {
+		toggleNav(!navState);
+		window.scrollTo({
+			top: ref ? (ref.current.id === 'services' ? ref.current.offsetTop + 900 : ref.current.offsetTop) : 0,
+			left: 0,
+			behavior: 'smooth',
+		});
+	};
 
-  const mobileMenu = (
-    <svg viewBox="0 0 100 100" class="menu">
-      <g>
-        <path d="M 0 50 L100 50 Z" stroke-width="2px" stroke="#fff" />
-        <path d="M 0, 30 L100, 30 Z" stroke-width="2px" stroke="#fff" />
-        <path d="M 0, 70 L100, 70 Z" stroke-width="2px" stroke="#fff" />
-      </g>
-    </svg>
-  );
+	const mobileMenu = (
+		<svg viewBox="0 0 100 100" class="menu" onClick={() => toggleNav(!navState)}>
+			<g>
+				<path
+					d="M 0 50 L100 50 Z"
+					stroke-width="3px"
+					stroke="#fff"
+					className={navState ? navStyles.rotated : ''}
+				/>
+				<path
+					d="M 0, 30 L100, 30 Z"
+					stroke-width="3px"
+					stroke="#fff"
+					className={navState ? navStyles.rotated : ''}
+				/>
+				<path
+					d="M 0, 70 L100, 70 Z"
+					stroke-width="3px"
+					stroke="#fff"
+					className={navState ? navStyles.rotated : ''}
+				/>
+			</g>
+		</svg>
+	);
 
-  const navList = (
-    <ul>
-      <li
-        onClick={() =>
-          scrollToSections(refs.filter(ref => ref.current.id === 'services')[0])
-        }
-        className={active === 'services' ? navStyles.active : ''}
-      >
-        <a>Services</a>
-      </li>
-      <li
-        onClick={() =>
-          scrollToSections(refs.filter(ref => ref.current.id === 'work')[0])
-        }
-        className={active === 'work' ? navStyles.active : ''}
-      >
-        <a>Work</a>
-      </li>
-      <li
-        onClick={() =>
-          scrollToSections(refs.filter(ref => ref.current.id === 'contact')[0])
-        }
-        className={active === 'contact' ? navStyles.active : ''}
-      >
-        <a>Contact</a>
-      </li>
-    </ul>
-  );
+	const navList = (
+		<ul>
+			<li
+				onClick={() => scrollToSections(refs.filter(ref => ref.current.id === 'services')[0])}
+				className={active === 'services' ? navStyles.active : ''}
+			>
+				<a>Services</a>
+			</li>
+			<li
+				onClick={() => scrollToSections(refs.filter(ref => ref.current.id === 'work')[0])}
+				className={active === 'work' ? navStyles.active : ''}
+			>
+				<a>Work</a>
+			</li>
+			<li
+				onClick={() => scrollToSections(refs.filter(ref => ref.current.id === 'contact')[0])}
+				className={active === 'contact' ? navStyles.active : ''}
+			>
+				<a>Contact</a>
+			</li>
+		</ul>
+	);
 
-  return (
-    <nav className={navStyles.nav}>
-      <div className={navStyles.nav__left}>
-        <a>AlexRoth productions</a>
-      </div>
-      {isMobile ? (
-        <div className={navStyles.mobile_nav__right}>{mobileMenu}</div>
-      ) : (
-        <div className={navStyles.nav__right}>{navList}</div>
-      )}
-    </nav>
-  );
+	const sideMenu = (
+		<div className={navState ? `${navStyles.side_menu}` : `${navStyles.side_menu} ${navStyles.hidden}`}>
+			{navList}
+			<div className={navStyles.side_menu_close} onClick={() => toggleNav(!navState)}></div>
+		</div>
+	);
+
+	console.log(navState);
+	return (
+		<nav className={navStyles.nav}>
+			<div className={navStyles.nav__left}>
+				<a>AlexRoth productions</a>
+			</div>
+			{isMobile ? (
+				<>
+					<div className={navStyles.mobile_nav__right}>{mobileMenu}</div>
+					{sideMenu}
+				</>
+			) : (
+				<div className={navStyles.nav__right}>{navList}</div>
+			)}
+		</nav>
+	);
 };
 
 const mapStateToProps = state => {
-  return {
-    refs: state.refs,
-    active: state.active
-  };
+	return {
+		refs: state.refs,
+		active: state.active,
+	};
 };
 
 export default connect(mapStateToProps, null)(Nav);
--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -6,122 +6,110 @@ import { servicesArray } from './servicesArray';
 import ServiceImgsSlide from './ServiceImgsSlide';
 
 const Services = ({ addRef, setActive }) => {
-  const serviceSvg = (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-      <path
-        fill="#400082"
-        fill-opacity="1"
-        d="M0,192L60,170.7C120,149,240,107,360,101.3C480,96,600,128,720,154.7C840,181,960,203,1080,186.7C1200,171,1320,117,1380,90.7L1440,64L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"
-      ></path>
-    </svg>
-  );
-  const [currentIndex, setIndex] = useState(0);
+	const serviceSvg = (
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+			<path
+				fill="#400082"
+				fill-opacity="1"
+				d="M0,192L60,170.7C120,149,240,107,360,101.3C480,96,600,128,720,154.7C840,181,960,203,1080,186.7C1200,171,1320,117,1380,90.7L1440,64L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"
+			></path>
+		</svg>
+	);
+	const [currentIndex, setIndex] = useState(0);
 
-  const [transition, setTransition] = useState(false);
-  const changeIndex = index => setIndex(index);
+	const [transition, setTransition] = useState(false);
+	const changeIndex = index => setIndex(index);
 
-  const serviceRef = useRef();
+	const serviceRef = useRef();
 
-  useEffect(() => {
-    setTransition(true);
+	useEffect(() => {
+		setTransition(true);
 
-    addRef(serviceRef);
+		addRef(serviceRef);
 
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) {
-          console.log(entry);
-          setActive(serviceRef.current.id);
-        }
-      },
-      { rootMargin: '0px 0px -200px 0px', threshold: 0.5 }
-    );
-    if (serviceRef.current) {
-      observer.observe(serviceRef.current);
-    }
+		const observer = new IntersectionObserver(
+			([entry]) => {
+				if (entry.isIntersecting) {
+					console.log(entry);
+					setActive(serviceRef.current.id);
+				}
+			},
+			{ rootMargin: '0px 0px -200px 0px', threshold: 0.5 }
+		);
+		if (serviceRef.current) {
+			observer.observe(serviceRef.current);
+		}
 
-    setTimeout(() => {
-      setTransition(false);
-    }, 1000);
+		setTimeout(() => {
+			setTransition(false);
+		}, 1000);
 
-    return () => clearTimeout();
-  }, [currentIndex, addRef, serviceRef]);
+		return () => clearTimeout();
+	}, [currentIndex, addRef, serviceRef, setActive]);
 
-  const services = servicesArray.map((service, i) => {
-    return (
-      <div className={serviceStyle.grid}>
-        <div className={serviceStyle.col}>
-          <div
-            className={serviceStyle.col_grid}
-            style={i % 2 !== 0 ? { background: 'transparent' } : null}
-          >
-            <div
-              className={serviceStyle.inner_left}
-              style={i % 2 !== 0 ? { order: '1' } : { order: '0' }}
-            >
-              <h3>{service.title}</h3>
-              <p>{service.text}</p>
-            </div>
-            <div
-              className={
-                service.imgs !== null
-                  ? serviceStyle.inner__img__grid
-                  : serviceStyle.inner_right
-              }
-            >
-              {service.imgs !== null ? (
-                <>
-                  <div class={serviceStyle.grid__inner}>
-                    <img
-                      src={service.imgs[currentIndex]}
-                      alt="website layout"
-                      className={transition ? serviceStyle.current_img : ''}
-                      key={currentIndex}
-                    />
-                    <ServiceImgsSlide
-                      imgs={service.imgs}
-                      onClick={changeIndex}
-                      currentIndex={currentIndex}
-                      serviceStyle={serviceStyle}
-                      transition={transition}
-                    />
-                  </div>
-                  <div className={serviceStyle.currentindex__marker}>
-                    {service.imgs.map((item, i) => (
-                      <span
-                        className={
-                          currentIndex === i ? serviceStyle.active__span : null
-                        }
-                      ></span>
-                    ))}
-                  </div>
-                </>
-              ) : service.img !== null ? (
-                service.img
-              ) : (
-                service.icons.map(icon => icon.icon)
-              )}
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  });
-  return (
-    <section className={serviceStyle.section}>
-      {serviceSvg}
-      <div className={serviceStyle.heading} ref={serviceRef} id="services">
-        <h2>Need a more impressive web presence?</h2>
-      </div>
-      {services}
-    </section>
-  );
+	const services = servicesArray.map((service, i) => {
+		return (
+			<div className={serviceStyle.grid}>
+				<div className={serviceStyle.col}>
+					<div className={serviceStyle.col_grid} style={i % 2 !== 0 ? { background: 'transparent' } : null}>
+						<div className={serviceStyle.inner_left} style={i % 2 !== 0 ? { order: '1' } : { order: '0' }}>
+							<h3>{service.title}</h3>
+							<p>{service.text}</p>
+						</div>
+						<div
+							className={service.imgs !== null ? serviceStyle.inner__img__grid : serviceStyle.inner_right}
+						>
+							{service.imgs !== null ? (
+								<>
+									<div class={serviceStyle.grid__inner}>
+										<img
+											src={service.imgs[currentIndex]}
+											alt="website layout"
+											className={transition ? serviceStyle.current_img : ''}
+											key={currentIndex}
+										/>
+										<ServiceImgsSlide
+											imgs={service.imgs}
+											onClick={changeIndex}
+											currentIndex={currentIndex}
+											serviceStyle={serviceStyle}
+											transition={transition}
+										/>
+									</div>
+									<div className={serviceStyle.currentindex__marker}>
+										{service.imgs.map((item, i) => (
+											<span
+												className={currentIndex === i ? serviceStyle.active__span : null}
+											></span>
+										))}
+									</div>
+								</>
+							) : service.img !== null ? (
+								service.img
+							) : (
+								service.icons.map(icon => icon.icon)
+							)}
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	});
+	return (
+		<section className={serviceStyle.section}>
+			{serviceSvg}
+			<div className={serviceStyle.heading} ref={serviceRef} id="services">
+				<h2>Need a more impressive web presence?</h2>
+			</div>
+			{services}
+		</section>
+	);
 };
 
 const mapStateToProps = state => {
-  return {
-    refs: state.refs
-  };
+	return {
+		refs: state.refs,
+	};
 };
 
 export default connect(mapStateToProps, { addRef, setActive })(Services);
