d115383c0671c5c66460ffab7985c0193f6b3e59
Alex Rothenberg <alexroth96@gmail.com>
2020-04-03 12:17:13 -0400

    solve header svg issue

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,6 +4,15 @@ import { headerSvg, bgWaveSvg } from './headerSvg';
 import { setActive } from '../../actions/refs';
 import { connect } from 'react-redux';
 const Header = ({ refs: { refs }, setActive }) => {
+	const waveSvg = (
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+			<path
+				fill="#380070"
+				fill-opacity="1"
+				d="M0,192L60,170.7C120,149,240,107,360,101.3C480,96,600,128,720,154.7C840,181,960,203,1080,186.7C1200,171,1320,117,1380,90.7L1440,64L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"
+			></path>
+		</svg>
+	);
 	const scrollToSections = refs => {
 		const ref = refs[0];
 		window.scrollTo({
@@ -59,9 +68,10 @@ const Header = ({ refs: { refs }, setActive }) => {
 						</div>
 					</div>
 				</div>
-				<div className={headerStyle.bg_svg}>{bgWaveSvg}</div>
+
 				<div className={headerStyle.svg__container}>{headerSvg}</div>
 			</div>
+			<div className={headerStyle.bg_svg}>{bgWaveSvg}</div>
 		</header>
 	);
 };
