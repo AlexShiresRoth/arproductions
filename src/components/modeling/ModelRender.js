a399d24d76470a00910c4e3a6120e7d9fab21bb4
Alex Roth <alexroth96@gmail.com>
2020-05-29 11:02:34 -0400

    add more animations

--- src/components/modeling/ModelRender.js
+++ src/components/modeling/ModelRender.js
@@ -1,134 +1,159 @@
 import React, { useEffect, useRef, useState } from 'react';
-import PropTypes from 'prop-types';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import modelStyle from './ModelRenderer.module.scss';
+import { handleIO } from '../customfunctions/handleIO';
 
 const ModelRender = (props) => {
-	let modelerRef = useRef();
+  let modelerRef = useRef();
 
-	const [initial, addToScene] = useState({
-		scene: {},
-		camera: {},
-		model: {},
-		renderer: {},
-		controls: {},
-	});
+  const modelRef = useRef();
 
-	const { scene, camera, model, renderer, controls } = initial;
+  const [initial, addToScene] = useState({
+    scene: {},
+    camera: {},
+    model: {},
+    renderer: {},
+    controls: {},
+  });
 
-	const sceneSetup = () => {
-		if (modelerRef) {
-			const width = modelerRef.clientWidth;
-			const height = modelerRef.clientHeight;
-			console.log(width, height);
-			let newScene = new THREE.Scene();
+  const { scene, camera, model, renderer, controls } = initial;
 
-			let newCamera = new THREE.PerspectiveCamera(
-				75, //Field of view
-				width / height,
-				0.01,
-				1000
-			);
+  const sceneSetup = () => {
+    if (modelerRef) {
+      const width = modelerRef.clientWidth;
+      const height = modelerRef.clientHeight;
+      console.log(width, height);
+      let newScene = new THREE.Scene();
 
-			let newControls = new OrbitControls(newCamera, modelerRef);
-			newCamera.position.z = 1;
-			newCamera.position.y = 0;
-			newCamera.position.x = 0;
+      let newCamera = new THREE.PerspectiveCamera(
+        75, //Field of view
+        width / height,
+        0.01,
+        1000
+      );
 
-			let newRenderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-			newRenderer.setSize(width, height);
-			modelerRef.appendChild(newRenderer.domElement);
+      let newControls = new OrbitControls(newCamera, modelerRef);
+      newCamera.position.z = 1;
+      newCamera.position.y = 0;
+      newCamera.position.x = 0;
 
-			addToScene((prev) => ({
-				...prev,
-				scene: newScene,
-				camera: newCamera,
-				renderer: newRenderer,
-				controls: newControls,
-			}));
-		}
-	};
+      let newRenderer = new THREE.WebGLRenderer({
+        antialias: true,
+        alpha: true,
+      });
+      newRenderer.setSize(width, height);
+      modelerRef.appendChild(newRenderer.domElement);
 
-	const addCustomSceneObject = () => {
-		const geometry = new THREE.BoxGeometry(2, 2, 2);
-		const material = new THREE.MeshPhongMaterial({
-			color: 0x156289,
-			emissive: 0x072534,
-			side: THREE.DoubleSide,
-			flatShading: true,
-		});
-		const loader = new GLTFLoader();
-		loader.load(
-			'models/gun-prototype.glb',
-			function (gltf) {
-				addToScene((prev) => ({ ...prev, model: gltf.scene, scene: prev.scene.add(gltf.scene) }));
-			},
-			undefined,
-			function (error) {
-				console.error(error);
-			}
-		);
+      addToScene((prev) => ({
+        ...prev,
+        scene: newScene,
+        camera: newCamera,
+        renderer: newRenderer,
+        controls: newControls,
+      }));
+    }
+  };
 
-		let newCube = new THREE.Mesh(geometry, material);
+  const addCustomSceneObject = () => {
+    const geometry = new THREE.BoxGeometry(2, 2, 2);
+    const material = new THREE.MeshPhongMaterial({
+      color: 0x156289,
+      emissive: 0x072534,
+      side: THREE.DoubleSide,
+      flatShading: true,
+    });
+    const loader = new GLTFLoader();
+    loader.load(
+      'models/gun-prototype.glb',
+      function (gltf) {
+        addToScene((prev) => ({
+          ...prev,
+          model: gltf.scene,
+          scene: prev.scene.add(gltf.scene),
+        }));
+      },
+      undefined,
+      function (error) {
+        console.error(error);
+      }
+    );
 
-		const lights = [];
-		lights[0] = new THREE.PointLight(0x41057e, 5);
-		lights[1] = new THREE.PointLight(0x4169e1, 10);
-		lights[2] = new THREE.PointLight(0x4169e1, 10);
+    let newCube = new THREE.Mesh(geometry, material);
 
-		lights[0].position.set(0, 10, 0);
-		lights[1].position.set(10, 10, 10);
-		lights[2].position.set(-10, -10, -10);
+    const lights = [];
+    lights[0] = new THREE.PointLight(0x41057e, 5);
+    lights[1] = new THREE.PointLight(0x4169e1, 10);
+    lights[2] = new THREE.PointLight(0x4169e1, 10);
 
-		addToScene((prev) => ({ ...prev, model: newCube, scene: prev.scene.add(...lights) }));
-	};
+    lights[0].position.set(0, 10, 0);
+    lights[1].position.set(10, 10, 10);
+    lights[2].position.set(-10, -10, -10);
 
-	useEffect(() => {
-		const startAnimationLoop = () => {
-			if (renderer.render) {
-				model.rotation.x -= 0.001;
-				model.rotation.y += 0.001;
-				renderer.render(scene, camera);
-			}
-			window.requestAnimationFrame(startAnimationLoop);
-		};
-		startAnimationLoop();
-	}, [renderer, camera, scene, model]);
+    addToScene((prev) => ({
+      ...prev,
+      model: newCube,
+      scene: prev.scene.add(...lights),
+    }));
+  };
 
-	useEffect(() => {
-		sceneSetup();
-		addCustomSceneObject();
-	}, []);
+  useEffect(() => {
+    const startAnimationLoop = () => {
+      if (renderer.render) {
+        model.rotation.x -= 0.001;
+        model.rotation.y += 0.001;
+        renderer.render(scene, camera);
+      }
+      window.requestAnimationFrame(startAnimationLoop);
+    };
+    startAnimationLoop();
+  }, [renderer, camera, scene, model]);
 
-	useEffect(() => {
-		const handleResize = () => {
-			if (modelerRef) {
-				const width = modelerRef.clientWidth;
-				const height = modelerRef.clientHeight;
+  useEffect(() => {
+    sceneSetup();
+    addCustomSceneObject();
+  }, []);
 
-				renderer.setSize(width, height);
-				camera.aspect = width / height;
-				camera.updateProjectionMatrix();
-				console.log(modelerRef);
-			}
-		};
-		window.addEventListener('resize', handleResize);
-		window.addEventListener('onload', handleResize);
-		return () => window.removeEventListener('resize', handleResize);
-	}, [modelerRef, camera, renderer]);
+  useEffect(() => {
+    const handleResize = () => {
+      if (modelerRef) {
+        const width = modelerRef.clientWidth;
+        const height = modelerRef.clientHeight;
 
-	return (
-		<section className={modelStyle.canvas}>
-			<div className={modelStyle.heading}>
-				<h2>3D Modeling</h2>
-				<p>Scifi gun game asset prototype.</p>
-				<p>Use your cursor or fingers to move the scene around!</p>
-			</div>
-			<div className={modelStyle.model_container} ref={(ref) => (modelerRef = ref)}></div>
-		</section>
-	);
+        renderer.setSize(width, height);
+        camera.aspect = width / height;
+        camera.updateProjectionMatrix();
+        console.log(modelerRef);
+      }
+    };
+    window.addEventListener('resize', handleResize);
+    window.addEventListener('onload', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [modelerRef, camera, renderer]);
+
+  const [intersecting, setIntersecting] = useState(false);
+
+  useEffect(() => {
+    handleIO(modelRef, 0, 0, setIntersecting);
+  }, [modelRef, setIntersecting]);
+
+  return (
+    <section className={modelStyle.canvas}>
+      <div
+        className={intersecting ? modelStyle.heading : modelStyle.out_of_view}
+        ref={modelRef}
+      >
+        <h2>3D Modeling</h2>
+        <p>Scifi gun game asset prototype.</p>
+        <p>Use your cursor or fingers to move the scene around!</p>
+      </div>
+      <div
+        className={modelStyle.model_container}
+        ref={(ref) => (modelerRef = ref)}
+      ></div>
+    </section>
+  );
 };
 
 ModelRender.propTypes = {};
--- src/components/work/WorkMap.js
+++ src/components/work/WorkMap.js
@@ -2,90 +2,107 @@ import React, { useState, useEffect, useRef } from 'react';
 import workStyle from './style/WorkMap.module.scss';
 import { workArray } from './workArray';
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
-import IntersectionObserver from 'intersection-observer-polyfill';
+import { handleIO } from '../customfunctions/handleIO';
+
 const WorkMap = (_) => {
-	const [start, setStart] = useState(0);
-	const [isMobile, setMobile] = useState(window.innerWidth <= 900);
-	const [end, setEnd] = useState(6);
-	const [shuffledArr, shuffleArray] = useState([]);
+  const [start, setStart] = useState(0);
 
-	useEffect(() => {
-		setStart(Math.floor(Math.random() * workArray.length));
-	}, []);
+  const [isMobile, setMobile] = useState(window.innerWidth <= 900);
 
-	useEffect(() => {
-		const handleShuffleArray = () => {
-			const randomArr = [];
+  const [end, setEnd] = useState(6);
 
-			let num = start;
-			workArray.forEach((item, i, arr) => {
-				num = num >= 0 && num < arr.length - 1 ? (num += 1) : 0;
+  const [shuffledArr, shuffleArray] = useState([]);
 
-				randomArr.push(arr[num]);
-			});
-			return shuffleArray(randomArr);
-		};
+  const [intersecting, setIntersecting] = useState(false);
 
-		handleShuffleArray();
-	}, [start]);
+  useEffect(() => {
+    setStart(Math.floor(Math.random() * workArray.length));
+  }, []);
 
-	useEffect(() => {
-		const handleResize = () => {
-			setMobile(window.innerWidth <= 900);
-		};
-		window.addEventListener('resize', handleResize);
-		return () => window.removeEventListener('resize', handleResize);
-	}, [isMobile]);
+  useEffect(() => {
+    const handleShuffleArray = () => {
+      const randomArr = [];
 
-	useEffect(() => {
-		isMobile ? setEnd(workArray.length) : setEnd(6);
-	}, [isMobile]);
+      let num = start;
+      workArray.forEach((item, i, arr) => {
+        num = num >= 0 && num < arr.length - 1 ? (num += 1) : 0;
 
-	const itemRef = useRef(null);
+        randomArr.push(arr[num]);
+      });
+      return shuffleArray(randomArr);
+    };
 
-	const [inView, setView] = useState(false);
-	useEffect(() => {
-		const observer = new IntersectionObserver(
-			([entry]) => {
-				if (entry.intersectionRatio > 0.2) {
-					console.log(entry);
-					setView(true);
-					observer.unobserve(entry.target);
-				}
-			},
-			{ rootMargin: '0px 0px -200px 0px', threshold: 0.4 }
-		);
-		if (itemRef.current) {
-			observer.observe(itemRef.current);
-		}
+    handleShuffleArray();
+  }, [start]);
 
-		return () => observer.disconnect();
-	}, [setView]);
+  useEffect(() => {
+    const handleResize = () => {
+      setMobile(window.innerWidth <= 900);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [isMobile]);
 
-	const works = shuffledArr.slice(0, end).map((item, i) => {
-		return (
-			<a href={item.url} target="_blank" rel="noopener noreferrer" className={workStyle.item} key={i}>
-				<img src={item.src} alt={item.title}></img>
-			</a>
-		);
-	});
+  useEffect(() => {
+    isMobile ? setEnd(workArray.length) : setEnd(6);
+  }, [isMobile]);
 
-	return (
-		<section className={workStyle.section}>
-			<div className={inView ? workStyle.heading : workStyle.out_of_view} ref={itemRef}>
-				<button onClick={(e) => setStart((prev) => (prev <= 0 ? (prev = workArray.length - 1) : prev - 1))}>
-					<IoIosArrowBack />
-				</button>
-				<p>
-					Total Projects: <span>{workArray.length}</span>
-				</p>
-				<button onClick={(e) => setStart((prev) => (prev < workArray.length - 1 ? prev + 1 : (prev = 0)))}>
-					<IoIosArrowForward />
-				</button>
-			</div>
-			<div className={inView ? workStyle.work__grid : workStyle.out_of_view}>{works}</div>
-		</section>
-	);
+  const itemRef = useRef(null);
+
+  //handle if section is intersecting
+  useEffect(() => {
+    handleIO(itemRef, 0, 0, setIntersecting);
+  }, [itemRef, setIntersecting]);
+
+  const works = shuffledArr.slice(0, end).map((item, i) => {
+    return (
+      <a
+        href={item.url}
+        target="_blank"
+        rel="noopener noreferrer"
+        className={workStyle.item}
+        key={i}
+      >
+        <img src={item.src} alt={item.title}></img>
+      </a>
+    );
+  });
+
+  return (
+    <section className={workStyle.section}>
+      <div
+        className={intersecting ? workStyle.heading : workStyle.out_of_view}
+        ref={itemRef}
+      >
+        <button
+          onClick={(e) =>
+            setStart((prev) =>
+              prev <= 0 ? (prev = workArray.length - 1) : prev - 1
+            )
+          }
+        >
+          <IoIosArrowBack />
+        </button>
+        <p>
+          Total Projects: <span>{workArray.length}</span>
+        </p>
+        <button
+          onClick={(e) =>
+            setStart((prev) =>
+              prev < workArray.length - 1 ? prev + 1 : (prev = 0)
+            )
+          }
+        >
+          <IoIosArrowForward />
+        </button>
+      </div>
+      <div
+        className={intersecting ? workStyle.work__grid : workStyle.out_of_view}
+      >
+        {works}
+      </div>
+    </section>
+  );
 };
 
 export default WorkMap;
